40f17d1a4f064ef46fa72649e6f7b4214316f1fb
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-06-18 12:20:33 +0900

    ビルドエラーを修正

--- src/main/scala/everywhere/e300nexys4ddrdevkit/FPGAChip.scala
+++ src/main/scala/everywhere/e300nexys4ddrdevkit/FPGAChip.scala
@@ -38,7 +38,7 @@ class E300Nexys4DDRDevKitFPGAChip(implicit override val p: Parameters) extends N
   // DUT
   //-----------------------------------------------------------------------
 
-  withClockAndReset(clock_32MHz, ck_rst) {
+  withClockAndReset(clock_32MHz, ~ck_rst) {
     val dut = Module(new E300Nexys4DDRDevKitPlatform)
 
     //---------------------------------------------------------------------
@@ -78,62 +78,6 @@ class E300Nexys4DDRDevKitFPGAChip(implicit override val p: Parameters) extends N
     // debug reset
     dut_ndreset := dut.io.ndreset
 
-    //---------------------------------------------------------------------
-    // Assignment to package pins
-    //---------------------------------------------------------------------
-    // Pins IO0-IO13
-    //
-    // FTDI UART TX/RX are not connected to ck_io[0,1]
-    // the way they are on Arduino boards.  We copy outgoing
-    // data to both places, switch 3 (sw[3]) determines whether
-    // input to UART comes from FTDI chip or gpio_16 (shield pin PD0)
-
-    val iobuf_ck0 = Module(new IOBUF())
-    iobuf_ck0.io.I := dut.io.pins.gpio.pins(16).o.oval
-    iobuf_ck0.io.T := ~dut.io.pins.gpio.pins(16).o.oe
-    attach(iobuf_ck0.io.IO, ck_io(0))   // UART0 RX
-
-    val iobuf_uart_txd = Module(new IOBUF())
-    iobuf_uart_txd.io.I := dut.io.pins.gpio.pins(16).o.oval
-    iobuf_uart_txd.io.T := ~dut.io.pins.gpio.pins(16).o.oe
-    attach(iobuf_uart_txd.io.IO, uart_txd_in)
-
-    // gpio(16) input is shared between FTDI TX pin and the Arduino shield pin using SW[3]
-    val sw_3_in = IOBUF(sw_3)
-    dut.io.pins.gpio.pins(16).i.ival := Mux(sw_3_in,
-                                            iobuf_ck0.io.O & dut.io.pins.gpio.pins(16).o.ie,
-                                            iobuf_uart_txd.io.O & dut.io.pins.gpio.pins(16).o.ie)
-
-    IOBUF(uart_rxd_out, dut.io.pins.gpio.pins(17))
-
-    // Shield header row 0: PD2-PD7
-    IOBUF(ck_io(2),  dut.io.pins.gpio.pins(18))
-    IOBUF(ck_io(3),  dut.io.pins.gpio.pins(19)) // PWM1(1)
-    IOBUF(ck_io(4),  dut.io.pins.gpio.pins(20)) // PWM1(0)
-    IOBUF(ck_io(5),  dut.io.pins.gpio.pins(21)) // PWM1(2)
-    IOBUF(ck_io(6),  dut.io.pins.gpio.pins(22)) // PWM1(3)
-    IOBUF(ck_io(7),  dut.io.pins.gpio.pins(23))
-
-    // Header row 1: PB0-PB5
-    IOBUF(ck_io(8),  dut.io.pins.gpio.pins(0))  // PWM0(0)
-    IOBUF(ck_io(9),  dut.io.pins.gpio.pins(1))  // PWM0(1)
-    IOBUF(ck_io(10), dut.io.pins.gpio.pins(2))  // SPI CS(0) / PWM0(2)
-    IOBUF(ck_io(11), dut.io.pins.gpio.pins(3))  // SPI MOSI  / PWM0(3)
-    IOBUF(ck_io(12), dut.io.pins.gpio.pins(4))  // SPI MISO
-    IOBUF(ck_io(13), dut.io.pins.gpio.pins(5))  // SPI SCK
-
-    dut.io.pins.gpio.pins(6).i.ival  := 0.U
-    dut.io.pins.gpio.pins(7).i.ival  := 0.U
-    dut.io.pins.gpio.pins(8).i.ival  := 0.U
-
-    // Header row 3: A0-A5 (we don't support using them as analog inputs)
-    // just treat them as regular digital GPIOs
-    IOBUF(ck_io(15), dut.io.pins.gpio.pins(9))  // A1 = CS(2)
-    IOBUF(ck_io(16), dut.io.pins.gpio.pins(10)) // A2 = CS(3) / PWM2(0)
-    IOBUF(ck_io(17), dut.io.pins.gpio.pins(11)) // A3 = PWM2(1)
-    IOBUF(ck_io(18), dut.io.pins.gpio.pins(12)) // A4 = PWM2(2) / SDA
-    IOBUF(ck_io(19), dut.io.pins.gpio.pins(13)) // A5 = PWM2(3) / SCL
-
     // Mirror outputs of GPIOs with PWM peripherals to RGB LEDs on Arty
     // assign RGB LED0 R,G,B inputs = PWM0(1,2,3) when iof_1 is active
     IOBUF(led0_r, dut.io.pins.gpio.pins(1))
@@ -145,11 +89,6 @@ class E300Nexys4DDRDevKitFPGAChip(implicit override val p: Parameters) extends N
     IOBUF(led1_r, dut.io.pins.gpio.pins(19))
     IOBUF(led1_g, dut.io.pins.gpio.pins(21))
     IOBUF(led1_b, dut.io.pins.gpio.pins(22))
-
-    // and RGB LED2 R,G,B inputs = PWM2(1,2,3) when iof_1 is active
-    IOBUF(led2_r, dut.io.pins.gpio.pins(11))
-    IOBUF(led2_g, dut.io.pins.gpio.pins(12))
-    IOBUF(led2_b, dut.io.pins.gpio.pins(13))
 
     // Only 19 out of 20 shield pins connected to GPIO pins
     // Shield pin A5 (pin 14) left unconnected
@@ -168,14 +107,6 @@ class E300Nexys4DDRDevKitFPGAChip(implicit override val p: Parameters) extends N
     // UART1 RX/TX pins are assigned to PMOD_D connector pins 0/1
     IOBUF(ja_0, dut.io.pins.gpio.pins(25)) // UART1 TX
     IOBUF(ja_1, dut.io.pins.gpio.pins(24)) // UART1 RX
-
-    // SPI2 pins mapped to 6 pin ICSP connector (standard on later
-    // arduinos) These are connected to some extra GPIO pins not connected
-    // on the HiFive1 board
-    IOBUF(ck_ss,   dut.io.pins.gpio.pins(26))
-    IOBUF(ck_mosi, dut.io.pins.gpio.pins(27))
-    IOBUF(ck_miso, dut.io.pins.gpio.pins(28))
-    IOBUF(ck_sck,  dut.io.pins.gpio.pins(29))
 
     // Use the LEDs for some more useful debugging things
     IOBUF(led_0, ck_rst)
